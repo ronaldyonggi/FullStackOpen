b34fda034a5adbe6866987c3df72c61ec41c508b
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-10-24 22:30:59 -0400

    completed 2.6-2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+    }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  // const [toShow, setToShow] = useState(persons)
+
+  // Adding a new Name
+  const addName = event => {
+    event.preventDefault()
+    let isExist = false; // flag that indicates whether the name is already present
+    persons.forEach(person => {
+      if (newName === person.name) isExist = true
+    })
+    if (isExist) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+
+  // Handle name input box change
+  const handleNameInputChange = event => setNewName(event.target.value)
+  // Handle number input box change
+  const handleNumberInputChange = event => setNewNumber(event.target.value)
+  // Handle filter input box change
+  const handleFilterChange = event => {
+    setNewFilter(event.target.value)
+  }
+  
+  // Filtered person to show
+  const filteredPersons = () => {
+    // If the filter input is empty, then by default show all
+    if (newFilter === '') return persons
+    return persons.filter(person => {
+      let personLowercase = person.name.toLowerCase()
+      let filterLowercase = newFilter.toLowerCase()
+      return personLowercase.includes(filterLowercase)
+    })
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameInputChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInputChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {filteredPersons().map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+    </div>
+  )
+}
+
+export default App;
