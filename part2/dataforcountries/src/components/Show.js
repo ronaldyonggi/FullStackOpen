9acf3187672f457dd0804391afe4c642d17d74d9
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-11-04 21:52:32 -0400

    case only match 1

--- part2/dataforcountries/src/components/Show.js
+++ part2/dataforcountries/src/components/Show.js
@@ -13,6 +13,35 @@ const Show = ({ countries, newFilter }) => {
             })
             // If length of filteredCountries is more than 10, return "too many matches"
             if (filteredCountries.length > 10) return <>Too many matches, specify another filter</>
+            // If there's only one matching country, display details
+            else if (filteredCountries.length === 1) {
+                let country = filteredCountries[0]
+                let countryName = country.name.common
+                let countryCapital = country.capital[0]
+                let countryArea = country.area
+                let countryFlag = country.flag
+
+                return (
+                    <div>
+                        <h1>{countryName}</h1>
+                        <div>Capital: {countryCapital}</div>
+                        <div>Area: {countryArea}</div>
+                        <h4>Languages:</h4>
+                        {Object.values(country.languages).map(language => 
+                            <div key={language}>
+                                {language}
+                            </div>)}
+                        <div>{countryFlag}</div>
+                    </div>
+                )
+            }
+            // Otherwise filtered countries are between 2 to 10. Simply list the countries
+            else {
+                let countryNames = []
+                for (let country of filteredCountries) {
+                    countryNames.push(country.name.common)
+                }
+            }
         }
     }
     return (
