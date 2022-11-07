8d2d677b6699da29a4c667a1ed006887b63c37d6
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-11-07 08:37:52 -0500

    added code for notes app

---
+++ lecture/notes/src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
