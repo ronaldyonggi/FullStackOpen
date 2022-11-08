829e1f4960f28fb8c75aab2fe7963664093dc9de
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-11-07 19:48:33 -0500

    add implementation for send data to server

--- lecture/notes/src/App.js
+++ lecture/notes/src/App.js
@@ -3,52 +3,60 @@ import axios from 'axios'
 import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState([]) // The notes resource
+  const [newNote, setNewNote] = useState('') // The input box
+  const [showAll, setShowAll] = useState(true) // Determines what to show
 
-useEffect(() => {
-  console.log('effect')
+  useEffect(() => {
+    // console.log('effect')
 
-  axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    })
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        // console.log('promise fulfilled')
+        setNotes(response.data)
+      })
 
-  // const eventHandler = response => {
-  //   console.log('promise fulfilled')
-  //   setNotes(response.data)
-  // }
+    // const eventHandler = response => {
+    //   console.log('promise fulfilled')
+    //   setNotes(response.data)
+    // }
 
-  // const promise = axios.get('http://localhost:3001/notes')
-  // promise.then(eventHandler)
-}, [])
+    // const promise = axios.get('http://localhost:3001/notes')
+    // promise.then(eventHandler)
+  }, [])
 
-  console.log('render', notes.length, 'notes')
+
+  // console.log('render', notes.length, 'notes')
 
   const handleNoteChange = event => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNote(event.target.value)
   }
   
   const addNote = event => {
     event.preventDefault()
     const noteObject = {
-      content: newNote,
+      content: newNote, // Grab the content from the input box
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5, // 50-50 chance of whether this new note is important
+      // id: notes.length + 1
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    // setNotes(notes.concat(noteObject))
+    // setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data)) // Add this note to the resource. 
+        setNewNote('') // Reset the input box back to empty
+      })
   }
+
   
-const notesToShow = showAll
-? notes
-: notes.filter(note => note.important === true)
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
 
   return (
     <div>
