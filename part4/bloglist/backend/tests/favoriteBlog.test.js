7dc016512770d4aecb1b32dd3c3f4180aecbe190
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-11 09:53:35 -0500

    completed 4.3-4.5

---
+++ part4/bloglist/backend/tests/favoriteBlog.test.js
@@ -0,0 +1,66 @@
+
+const listHelper = require('../utils/list_helper')
+
+describe('most likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const ansOneBlog =
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    }
+
+  test('when list has only one blog, return that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(ansOneBlog)
+  })
+
+  const multipleBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const answer =
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+    }
+
+  test('when list has multiple blogs, return the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(multipleBlogs)
+    expect(result).toEqual(answer)
+  })
+})
--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = blogs => {
+  return 1
+}
 
+const totalLikes = blogs => {
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  let currentFav = 0 // index of the blog with the most likes
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i]['likes'] > blogs[currentFav]['likes']) currentFav = i
+  }
+  return (({ title, author, likes }) => ({ title, author, likes }))(blogs[currentFav])
 }
 
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
