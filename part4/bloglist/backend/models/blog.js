4c11d79c153d6117fcf1176b622e711cadbe1b4d
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-11 12:59:06 -0500

    adjusted spacing

--- part4/bloglist/backend/models/blog.js
+++ part4/bloglist/backend/models/blog.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    author: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    url: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    likes: {
-        type: Number,
-        required: true
-    }
+  title: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  author: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  url: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true
+  }
 })
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 module.exports = mongoose.model('Blog', blogSchema)
