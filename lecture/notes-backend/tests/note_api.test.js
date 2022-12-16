4e3727c148e7f2ff9195ff06d7e7e32511850adf
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-16 14:09:54 -0500

    using Promise.all for initiate DB

--- lecture/notes-backend/tests/note_api.test.js
+++ lecture/notes-backend/tests/note_api.test.js
@@ -8,11 +8,23 @@ const Note = require('../models/note')
 beforeEach(async () => {
   // Clear the DB in the beginning
   await Note.deleteMany({})
+
+  const noteObjects = helper.initialNotes.map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+  // helper.initialNotes.forEach(async note => {
+  //   let noteObject = new Note(note)
+  //   await noteObject.save()
+  //   console.log('saved')
+  // })
+
+  console.log('done')
+
   // Add the 2 initial notes into the DB
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  // let noteObject = new Note(helper.initialNotes[0])
+  // await noteObject.save()
+  // noteObject = new Note(helper.initialNotes[1])
+  // await noteObject.save()
 })
 
 test('notes are returned as json', async () => {
