4554d172137e2904b4874e6af51b1cb3a36dd306
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-16 14:10:09 -0500

    added tests for blogs api

---
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'This is the first blog',
+    author: 'This is the first author',
+    url: 'This is the url',
+    likes: 34
+  },
+  {
+    title: 'Just another blog',
+    author: 'Just simply another author',
+    url: 'Just another URL',
+    likes: 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test ('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('This is the first blog')
+})
+
+
+
+afterAll(() => mongoose.connection.close())
