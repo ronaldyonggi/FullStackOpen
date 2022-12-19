6fce4698433b1681e2ccb850773c1a9d3db1a7ed
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-19 09:36:59 -0500

    put request uses async/await

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -38,7 +38,7 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const blog = {
@@ -48,11 +48,12 @@ blogsRouter.put('/:id', (request, response, next) => {
     likes: body.likes
   }
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).json(updatedBlog)
+  // .then(updatedBlog => {
+  //   response.json(updatedBlog)
+  // })
+  // .catch(error => next(error))
 })
 
 module.exports = blogsRouter
