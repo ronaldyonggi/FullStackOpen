26427b1d77b569a300c72492aed246b2d89bcced
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-12-22 14:21:52 -0500

    reverted back to section testing backend

--- lecture/notes-backend/app.js
+++ lecture/notes-backend/app.js
@@ -1,4 +1,3 @@
-const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
@@ -25,9 +24,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
 
-
+app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
